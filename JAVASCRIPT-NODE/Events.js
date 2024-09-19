e2ecb625bade79fd4f5bfa9af5d5cc4a6b4835b7
BillyJoseph kimani <133921772+billyjoseph1@users.noreply.github.com>
2024-09-19 10:37:24 +0300

    HTMLDOM

---
+++ JAVASCRIPT-NODE/Events.js
@@ -0,0 +1,7 @@
+//eg, onchange(An HTML has been changed), onclick(a usetr clicks an HTML element)
+//Examples
+//In HTML onclick is the event listener, myFunction is the event handler:
+// used in HTML
+<button onclick="myFunction()">Click me</button>
+//In JavaScript click is the event, myFunction is the event handler:
+button.addEventListener("click", myFunction);
---
+++ JAVASCRIPT-NODE/basicDom.js
@@ -0,0 +1,5 @@
+//DOM - DOCUMENT OBjECT MODEL
+//IT IS NOT PART OF JS 
+//IT IS AN API
+
+console.log(documment);// document is not defined
